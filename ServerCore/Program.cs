ef8869d23465810dd8886961733d012941328b3f
lateson6 <lateson6@gmail.com>
2022-08-11 16:19:32 +0900

    초기 커밋.

---
+++ ServerCore/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class SpinLock
+    {
+        volatile int _locked = 0;
+        //잠금 풀때까지 뺑뺑이
+        public void Acquire()
+        {
+            while (true)
+            {
+
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)
+                //    break;
+                //CAS compare-and-swap
+                int original = Interlocked.CompareExchange(ref _locked, 1, 0);
+                if (original == 0)
+                    break;
+            }
+        }
+        public void Release()
+        {
+            _locked = 0;
+
+        }
+    }
+    class Program
+    {
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
+        static void Thread_1()
+        {
+            for(int i = 0; i < 100000; i++)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
+        }
+        static void Thread_2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(_num);
+        }
+    }
+}
