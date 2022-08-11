3a67b9c544a317974434419e0c0b5cd88e82bd58
lateson6 <lateson6@gmail.com>
2022-08-11 20:19:01 +0900

    practicing Readerwriterlock

---
+++ ServerCore/Lock.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    //재귀적 lock 허용?(yes)->writeLock->writelock ok writelock->readlock ok readlock->writelock xx
+    //스핀락 정책(5000번->yield)
+
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+        //32bit.. [unused(1)] [writeThreadId(15bit)] [ReadCount(16bit)]
+        int _flag=EMPTY_FLAG;
+        int _writeCount = 0;
+
+        public void WriteLock()
+        {
+
+            //동일 쓰레드가 wrtielock을 이미 획득하고 잇는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                _writeCount++;
+                return;
+
+            }
+
+            //아무도 writeLock or ReadLock을 획득하고 있지 않을때, 경합해 소유권을 얻는다
+            while (true)
+            {
+                int desired=(Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+                for(int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    //시도할때성공시 return
+                    if(Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                    {
+                        _writeCount = 1;
+
+                        //성공
+                        return;
+                    }
+                    //이건 atomic 하지 않음.
+                    //if (_flag == EMPTY_FLAG)
+                    //    _flag = desired;
+                }
+
+                Thread.Yield();
+
+            }
+        }
+        public void WriteUnlock()
+        {
+            int lockCount = --_writeCount;
+            if (lockCount == 0)
+                Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+        public void ReadLock()
+        {//동일 쓰레드가 wrtielock을 이미 획득하고 잇는지 확인
+            int lockThreadId = (_flag & WRITE_MASK) >> 16;
+            if (Thread.CurrentThread.ManagedThreadId == lockThreadId)
+            {
+                Interlocked.Increment(ref _flag);
+                return;
+
+            }
+            //아무도 write lock을 획득하고 잇지 않으면, readcount를 1늘린다.
+            while (true)
+            {
+                for(int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if(Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                    {
+
+                        return;
+                    }
+
+                //    if ((_flag & WRITE_MASK) == 0){
+                //        _flag = _flag + 1;
+                //}
+                }
+                Thread.Yield();
+            }
+        }
+        public void ReadUnLock()
+        {
+            Interlocked.Decrement(ref _flag);
+
+        }
+    }
+}
---
+++ ServerCore/ReaderWriterLocks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    class ReaderWriterLocks
+    {
+        //상호배제
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        
+        class Reward
+        {
+
+        }
+        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
+        static Reward GetRewardById(int id)
+        {
+            _lock3.EnterReadLock();
+            _lock3.ExitReadLock();
+            lock (_lock)
+            {
+                //진짜진짜진짜가끔 일어남->그걸 위해 lock 잡는건 조금 비효율적
+                //특수한 경우 일어난 경우에만 막는걸 ReaderWriterLock이라함.
+            }
+            return null;
+        }
+        static void AddReward(Reward reward)
+        {
+            _lock3.EnterWriteLock();
+            _lock3.ExitWriteLock();
+
+        }
+        static void Main(string[] args)
+        {
+            //1. spinlock
+            //2. yield
+            //3. AutoResetEvent
+
+            bool lockTaken = false;
+            _lock2.Enter(ref lockTaken);
+
+            _lock2.Exit();
+        
+        }
+    }
+}
