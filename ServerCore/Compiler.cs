b5c9e5ed1a1c52c05d55136fc25d69527300db83
lateson6 <lateson6@gmail.com>
2022-08-12 19:02:36 +0900

    study Thread local Storage

---
+++ ServerCore/Compiler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Compiler
+    {
+        //static: 모든 쓰레드가 공유함. 
+        static bool _stop = false;
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작");
+            while (_stop == false)
+            {
+                //stop신호를 기다림 
+            }
+            Console.WriteLine("쓰레드 끝");
+
+
+        }
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
+            Thread.Sleep(1000);
+            _stop = true;
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기");
+            t.Wait();//Thread join과 같음
+            Console.WriteLine("종료");
+        }
+    }
+}
---
+++ ServerCore/Tls.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//일감이 많게 큐 저장시 하나만 꺼내는게 아니라 뭉탱이로 뽑아다가 내 공간에 넣어두고 사용
+namespace ServerCore
+{
+    class Tls
+    {
+        //lock없이 부담없이 쓸수있다. 내 영역이야!!
+        static ThreadLocal<string> ThreadName=new ThreadLocal<string>(()=> { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+        //static string ThreadName;
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat)
+            {
+
+                Console.Write(ThreadName.Value+"(repeat)");
+            }
+            else
+            {
+                Console.Write(ThreadName.Value);
+            }
+            //ThreadName.Value = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
+            //Thread.Sleep(1000);
+
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            //인자만큼 task 실행
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            ThreadName.Dispose();
+        }
+    }
+}
