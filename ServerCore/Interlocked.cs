149bba26d55e90bd3f9bb45d4b947a781f64cf90
lateson6 <lateson6@gmail.com>
2022-11-23 01:51:55 +0900

    Lock_Basic

--- ServerCore/Interlocked.cs
+++ ServerCore/Interlocked.cs
@@ -7,16 +7,22 @@ namespace ServerCore
 {
     class Interlock
     {
-
+        //컴파일러나 하드웨어 관련은 큰 문제 X
+        /*
+         * lock, atomic으로 충분히 해결
+         * 공유변수 접근에 대한 문제
+         */
         static int number = 0;
         static void Thread_1()
         {
-            
-            for(int i = 0; i < 10000; i++)
-            {
 
+            for (int i = 0; i < 10000; i++)
+            {
+                //특정 변수를 atomic하게 증가시키다. 성능 손해
+                //ref: number가 뭔지 알지 못하지만 참조하여 수치를 1 증가시킨다. 
                 Interlocked.Increment(ref number);
-                //number++; 
+                //컴파일러 최적화 문제는 발견되지 않음
+                //number++; 실행이 되거나, 실행이 안되거나
             }
         }
         static void Thread_2()
@@ -29,14 +35,39 @@ namespace ServerCore
         }
         static void Main(string[] args)
         {
-
+            /*
+             * 
+             * number++은 어떻게 작동하나?
+             * 어셈블리 코드로 가면 사실 3줄로 동작
+             * int temp=number;
+             * temp+=1;
+             * number=temp;
+             * 동시다발로 들어가면 
+             * 0으로 시작하다가 T1은 1, T2는 -1
+             * 누가 먼저 실행될지는 모름
+             * 단계별로 실행되므로 문제가 생기는것
+             * 원자성! atomic. CPU 한 단위에 작업이 처리되는것
+             * 골드-=100
+             * 인벤+=검->원자적으로 일어나야함  왜?
+             * 이사이에 서버 크래쉬나면 샀는데 검이 없는버그
+             * 
+             */
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
             t2.Start();
             Task.WaitAll(t1, t2);
             Console.WriteLine(number);
-
+            //잘 된다. 0
+            /*
+             * 10만번하면 0이 아니게 된다. 
+             * 경합 조건을 다뤄보자(Race condition
+             * 내 메모장에만 적지말고 주문현황판에도 업로드
+             * (다른 직원도 주문현황 볼 수있게)
+             * 콜라를 2번 테이블 - 냉장고에 꺼내주면됨
+             * 3명 직원이 각자 콜라를 하나씩 꺼내옴
+             * 2번테이블에 콜라 3개
+             */
         }
     }
 }
---
+++ ServerCore/Lock_Basic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace ServerCore
+{
+    class Lock_Basic
+    {
+        //1. 전역변수 메모리 값을 레지스터에 갖고오고, 레지스터 값을 증가시키고, 그 값을 메모리 값에 넣는다
+        static object _obj = new object();
+        static int number = 0;
+        static void Thread_1()
+        {
+
+            for (int i = 0; i < 10000; i++)
+            {
+                /*
+                 * Mutual Exclusive 상호배제
+                 */
+                Monitor.Enter(_obj);
+                number++;
+                //블록 내부는 사실상 single Thread다.
+                //동시 다발적으로 쓰레드 접근시 문제발생 Critical Section 임계영역
+                //Lock
+                //monitor exit을 안하고 나가버리면 영원히 잠겨버림
+                //Thread 2에서 접근을 못해 하염없이 대기
+                //데드락 상황
+                //수동으로 exit를 해줘야한다. 예외처리로 한다해도 번거로움
+                //직접 monitor로 하는 경우는 없고 lock을 쓸거임 
+                //
+                /*
+                 * lock(_obj){
+                 * number++; 끗
+                 * }
+                 * 끝나면 알아서 열어준다.
+                 */
+                Monitor.Exit(_obj);
+                //화장실에서 나간다 잠금을 푼다 
+                
+            }
+        }
+        static void Thread_2()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+
+                Monitor.Enter(_obj);
+                number--;
+                Monitor.Exit(_obj);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(number);
+        }
+    }
+}
